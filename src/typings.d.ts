4841d8dc3eab919b1aacce769e4921b2d27c4be6
Gabriel Bazán <gabrielbazan7@hotmail.com>
2017-07-28 16:48:12 -0300

    Auto-generated commit

--- src/typings.d.ts
+++
@@ -1,7 +0,0 @@
-/* SystemJS module definition */
-declare module 'd3' { let exportAs: any; export = exportAs; }
-declare module 'nv' { let exportAs: any; export = exportAs; }
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
